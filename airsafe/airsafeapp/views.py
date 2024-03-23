d218ed3ebc4b9609f539cd8f138bb686a716998e
rishikadeshmukh <157061074+rishikadeshmukh@users.noreply.github.com>
2024-03-23 15:41:58 -0400

    graphing data

--- airsafe/airsafeapp/views.py
+++ airsafe/airsafeapp/views.py
@@ -2,27 +2,26 @@ from django.http import HttpResponse
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import integrate
 
 
 def index(request):
     return HttpResponse("Hello! Welcome to the AirSafe App!")
 
+
 def interpolateBspline(x, y, xi):
     yi = []
     A = y
-
     for i in xi:
-        if x[0] <= i <= x[-1]:
-            index = 0
-            while x[index] < i:
-                index += 1
-            if x[index] == i:
-                yi.append(y[index])
-            else:
-                yi.append(y[index - 1] + (i - x[index - 1]) * (y[index] - y[index - 1]) / (x[index] - x[index - 1]))
+        #if x[0] <= i <= x[1]:
+        index = 0
+        while x[index] < i:
+            index += 1
+        if x[index] == i:
+            yi.append(y[index])
         else:
-            yi.append(None)
+            yi.append(y[index - 1] + (i - x[index - 1]) * (y[index] - y[index - 1]) / (x[index] - x[index - 1]))
+        # else:
+        #     yi.append(None)
     return yi, A
 
 
@@ -42,19 +41,18 @@ def interpolatePoly(x, y, xi):
 
     return yi, co
 
-df = pd.read_csv("cardiacMRIdata.csv")
-time = df.loc[:,"Time (ms)"]
-baseline = df.loc[:,"Baseline"]
-dobutamine = df.loc[:,"Dobutamine"]
 
-f, axes = plt.subplots(3,1)
-axes[0].plot(time,dobutamine,'.-')
+df = pd.read_csv("cardiacMRIdata.csv")
+time = df.loc[:, "Time (ms)"]
+baseline = df.loc[:, "Baseline"]
+dobutamine = df.loc[:, "Dobutamine"]
+
+f, axes = plt.subplots(3, 1)
+axes[0].plot(time, dobutamine, '.-')
 axes[0].set_ylabel('Volume')
 
 # calculates times at which to interpolate
-tInterpolated = np.linspace(np.min(time),np.max(time),100)
-
-
+tInterpolated = np.linspace(np.min(time), np.max(time), 100)
 yiP, aP = interpolatePoly(time, dobutamine, tInterpolated)
 axes[1].plot(tInterpolated, yiP, '.-')
 axes[1].set_ylabel('Volume')
@@ -62,3 +60,7 @@ axes[1].set_ylabel('Volume')
 yiB, aB = interpolateBspline(time, dobutamine, tInterpolated)
 axes[2].plot(tInterpolated, yiB, '.-')
 axes[2].set_ylabel('Volume')
+
+
+plt.show(block=True)
+plt.interactive(False)
