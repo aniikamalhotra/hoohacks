7a74f36f2c0735269a1c520031609a01bb0b27f4
rishikadeshmukh <157061074+rishikadeshmukh@users.noreply.github.com>
2024-03-23 15:44:47 -0400

    deleting duplicate directories

--- airsafe/medmodeling/asgi.py
+++ airsafe/medmodeling/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for hoohacks project.
+ASGI config for medmodeling project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hoohacks.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medmodeling.settings')
 
 application = get_asgi_application()
--- hoohacks/airsafe/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AirsafeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'airsafe'
