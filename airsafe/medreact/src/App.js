e28af036078b93d234b8141aa6b0e93bb81409b1
aniikamalhotra <gnn3yv@virginia.edu>
2024-03-23 21:23:46 -0400

    logged in verification and log out functionality

--- airsafe/medreact/src/App.js
+++ airsafe/medreact/src/App.js
@@ -1,17 +1,32 @@
 import logo from './logo.svg';
-import { useRedirectFunctions } from '@propelauth/react';
+import React from 'react';
+import { useLogoutFunction, withAuthInfo, useRedirectFunctions } from '@propelauth/react';
 import './App.css';
 
 
-function App() {
-  const {redirectToLoginPage} = useRedirectFunctions();
-  const color = '#4F4FAE';
-  return (
-    <div className="welcome-screen" style={{backgroundColor: color}}>
-            <h1 style={{color:"white"}}>Welcome to Medical Modeling!</h1>
-            <button className="login-button" onClick = {() => redirectToLoginPage()}>Login</button>
-    </div>
-  );
+function App({ isLoggedIn }) {
+    const logoutFn = useLogoutFunction();
+    const { redirectToSignupPage, redirectToLoginPage } = useRedirectFunctions();
+    const color = '#4F4FAE';
+
+    return (
+        <div className="welcome-screen" style={{ backgroundColor: color }}>
+            <h1 style={{ color: "white" }}>Welcome to Medical Modeling!</h1>
+            {isLoggedIn ? (
+                <div>
+                    The User is logged in
+                    <button onClick={() => logoutFn()}>Click here to log out</button>
+                </div>
+            ) : (
+                <div>
+                    To get started, please log in as a test user.
+                    <br />
+                    <button onClick={() => redirectToSignupPage()}>Sign up</button>
+                    <button className="login-button" onClick={() => redirectToLoginPage()}>Login</button>
+                </div>
+            )}
+        </div>
+    );
 }
 
-export default App;
+export default withAuthInfo(App);
