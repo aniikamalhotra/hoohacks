ea497717b1cbaa91524ee983b68e94e3d914dbc9
aliciafcarvalho <aliciafc934@gmail.com>
2024-03-24 02:37:45 -0400

    bootstrapping plot.html and back buttons

--- airsafe/airsafeapp/urls.py
+++ airsafe/airsafeapp/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 #importing home function from views.py
 from .views import home
 from . import views
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name = "base"),
     path('home/', home, name='home'),
     path("delete_item/<int:id>/", views.delete, name = "delete"),
     path("plot", views.plot, name ="plot")
