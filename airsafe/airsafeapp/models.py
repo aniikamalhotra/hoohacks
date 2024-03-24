c3108aea301e33f005ab5c26ab918bcadab95e9f
rishikadeshmukh <157061074+rishikadeshmukh@users.noreply.github.com>
2024-03-23 22:27:42 -0400

    user input functionality

--- airsafe/airsafeapp/models.py
+++ airsafe/airsafeapp/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class volume_at_time(models.Model):
-    time = models.CharField(max_length=10,default=0)
-    volume = models.CharField(max_length=10,default=0)
+    time = models.IntegerField(max_length=10,default=0)
+    volume = models.IntegerField(max_length=10,default=0)
 
 
 
