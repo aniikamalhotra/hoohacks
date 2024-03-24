b56a224a41ac4f53d7a75a116a2b362973bc88fe
aniikamalhotra <gnn3yv@virginia.edu>
2024-03-23 21:40:40 -0400

    log in screen functionality and design done

--- airsafe/medreact/src/App.js
+++ airsafe/medreact/src/App.js
@@ -11,22 +11,27 @@ function App({ isLoggedIn }) {
 
     return (
         <div className="welcome-screen" style={{ backgroundColor: color }}>
-            <h1 style={{ color: "white" }}>Welcome to Medical Modeling!</h1>
-            {isLoggedIn ? (
+            <div>
+                <h1 style={{ color: "white" }}>Welcome to Medical Modeling!</h1>
+            </div>
+            <div className="content">
                 <div>
-                    The User is logged in
-                    <button onClick={() => logoutFn()}>Click here to log out</button>
+                    {isLoggedIn ? (
+                        <div className="centered-container">
+                            <h3 style={{color: "white"}}>The User is logged in</h3>
+                            <button onClick={() => logoutFn()}>Click here to log out</button>
+                        </div>
+                    ) : (
+                        <div>
+                            <h3 style={{color: "white"}}>To get started, please log in or create a new account.</h3>
+                            <br />
+                            <button onClick={() => redirectToSignupPage()}>Sign up</button>
+                            <button className="login-button" onClick={() => redirectToLoginPage()}>Login</button>
+                        </div>
+                    )}
                 </div>
-            ) : (
-                <div>
-                    To get started, please log in as a test user.
-                    <br />
-                    <button onClick={() => redirectToSignupPage()}>Sign up</button>
-                    <button className="login-button" onClick={() => redirectToLoginPage()}>Login</button>
-                </div>
-            )}
+            </div>
         </div>
     );
 }
-
 export default withAuthInfo(App);
